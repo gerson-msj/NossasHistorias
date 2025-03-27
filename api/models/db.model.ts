ef176a35a83bd9985d426f74b8ee59d0aca947db
gerson-msj <gerson.msj@gmail.com>
2025-03-27 16:34:57 -0300

    1up

--- api/models/db.model.ts
+++ api/models/db.model.ts
@@ -4,3 +4,26 @@ export interface Usuario {
     DataCriacao: number;
     Moderador: number;
 }
+
+export type HistoriaSituacao = "Em analise" | "Aprovada" | "Reprovada";
+export const HistoriaSituacaoAnalise: HistoriaSituacao = "Em analise";
+export const HistoriaSituacaoAprovada: HistoriaSituacao = "Aprovada";
+export const HistoriaSituacaoReprovada: HistoriaSituacao = "Reprovada";
+
+export interface Historia {
+    Id: number;
+    IdUsuarioAutor: number;
+    IdUsuarioModerador: number;
+    Titulo: string;
+    Conteudo: string;
+    Situacao: HistoriaSituacao;
+    MotivoSituacao: string | null;
+    DataHoraCriacao: number;
+    DataHoraModeracao: number | null;
+}
+
+export interface Visualizacao {
+    IdUsuario: number;
+    IdHistoria: number;
+    curtida: boolean;
+}
